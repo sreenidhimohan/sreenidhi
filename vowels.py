d660cef59fb7264ff37603092f4eb60147209c55
sreenidhimohan <52481658+sreenidhimohan@users.noreply.github.com>
2019-07-14 06:36:33 -0700

    Update vowels.py

--- vowels.py
+++ vowels.py
@@ -1,4 +1,4 @@
-ch=int("enter the character")
+ch=int(input("enter the character")
 if(ch=='a'or ch=='A' or ch=='e' or ch=='E' or ch=='i' or ch=='I' or ch=='o' or ch=='O' or ch=='u' or ch=='U'):
 print("vowels")
 else:
